e6f8c93606229b9ee54f1fc1463b50b6d71948ce
vivek <vivekcse.in@gmail.com>
2025-07-15 22:14:47 +0530

    update

--- api/index.ts
+++ api/index.ts
@@ -1,50 +1,7 @@
 import app from '../src/app';
-import jwt from '@fastify/jwt';
-import cors from '@fastify/cors';
-import fastifyPlugin from 'fastify-plugin';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-// Export handler for Vercel
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-
-    await app.register(cors, {
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-        credentials: true
-    });
-
-    await app.register(jwt, {
-        secret: process.env.JWT_SECRET || ''
-    });
-
-    // Health check
-    app.get('/health', async (_req, reply) => {
-        // return { status: 'ok', timestamp: new Date().toISOString() };
-        reply.status(200)
-            .header("Content-Type", "application/json; charset=utf-8")
-            .send(new Date().toISOString() + ' Server is healthy');
-    });
-
-    //not found page app route
-    app.get("/*", (_req, reply) => {
-        reply.status(404)
-            .header("Content-Type", "application/json; charset=utf-8")
-            .send("Error 404, URL not found");
-    });
-
-    // Register plugins
-    await app.register(
-        fastifyPlugin((fastify, _opts, done) => {
-            //register all plugins
-            fastify.ready((err) => {
-                if (err) {
-                    console.error("Error connecting to any plugin:", err);
-                    process.exit(1);
-                }
-                console.log("Successfully connected to all plugins");
-            });
-            done();
-        })
-    );
-    await app.ready();
-    app.server.emit('request', req, res);
-};
+export default async function handler(req: VercelRequest, reply: VercelResponse) {
+    await app.ready()
+    app.server.emit('request', req, reply)
+}
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,75 @@
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 
-// api/index.js - Main Vercel serverless function
-import Fastify from 'fastify';
-// import cors from '@fastify/cors';
-// import jwt from '@fastify/jwt';
-
-// Create Fastify instance
 const app = Fastify({
-    logger: process.env.NODE_ENV === 'development'
+    logger: process.env.NODE_ENV === "development",
 });
 
-// // Register plugins
-// app.register(cors, {
-//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//     credentials: true
-// });
+app.get(
+    "/",
+    {
+        schema: {
+            response: {
+                200: {
+                    type: "string",
+                },
+            },
+        },
+    },
+    async (_req: FastifyRequest, reply: FastifyReply) => {
+        return reply.status(200).type("text/html").send(html);
+    }
+);
 
-// app.register(jwt, {
-//     secret: process.env.JWT_SECRET || ''
-// });
+// Health check
+app.get("/health",
+    {
+        schema: {
+            response: {
+                200: {
+                    type: "json/string",
+                },
+            },
+        },
+    },
+    async (_req, reply) => {
+        // return { status: 'ok', timestamp: new Date().toISOString() };
+        reply
+            .status(200)
+            .header("Content-Type", "application/json; charset=utf-8")
+            .send(new Date().toISOString() + " Server is healthy");
+    });
 
-// Routes
-// Register homepage app route
-app.get("/", async (_req, reply) => {
-    reply.status(200)
+//not found page app route
+app.get("/*", (_req, reply) => {
+    reply
+        .status(404)
         .header("Content-Type", "application/json; charset=utf-8")
-        .send("Server is fast with fastify");
+        .send("Error 404, URL not found");
 });
 
-
+export const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background: #000000;
+        text-align: center;
+    }
+    title{
+        padding :10px;
+    }
+    </style>
+    <title>Fastify Backend</title>
+  </head>
+  <body>
+    <h1>Vercel Fastify template by @vivekcsein</h1>
+  </body>
+</html>
+`;
 export default app;
