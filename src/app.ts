6cc0d1202dfc8902a77b1964f73e41195d0f15f4
vivek <vivekcse.in@gmail.com>
2025-07-16 16:32:22 +0530

    app .ts changed

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 export async function createApp(): Promise<FastifyInstance> {
     const app = Fastify({
-        logger: {
-            level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
-        },
+        // logger: {
+        //     level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
+        // },
     });
-
-    app.get('/', async (request, reply) => {
-        return reply.send({
-            success: true,
-            message: 'HomePage Eoute',
-        });
-
-    });
+    //homepage route
+    app.get('/', async (req, reply) => {
+        return reply.status(200).type('text/html').send(html)
+    })
 
     // Health check endpoint
     app.get('/api/health', async (request, reply) => {
@@ -41,3 +37,25 @@ export async function createApp(): Promise<FastifyInstance> {
 }
 
 export default createApp;
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+    />
+    <title>Vercel + Fastify Hello World</title>
+    <meta
+      name="description"
+      content="This is a starter template for Vercel + Fastify."
+    />
+  </head>
+  <body>
+    <h1>Vercel + Fastify Hello World</h1>
+  </body>
+</html>
+`
