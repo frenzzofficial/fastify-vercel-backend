1609c03a860b2d8a9d0b17a594ea1bfdc32df415
vivek <vivekcse.in@gmail.com>
2025-07-18 19:01:47 +0530

    vercel static file deploy

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,44 @@
-import Fastify, { FastifyInstance } from 'fastify';
-// import dotenv from 'dotenv';
+import dotenv from "dotenv";
+dotenv.config();
+import fastifyStatic from "@fastify/static";
+import Fastify, { FastifyInstance } from "fastify";
+import { Home, viewConfig } from "./libs/configs/config.views";
 
-// dotenv.config();
+const App = async (): Promise<FastifyInstance> => {
+  const app = Fastify({
+    logger: {
+      level: process.env.NODE_ENV === "production" ? "warn" : "info",
+    },
+  });
 
-export async function createApp(): Promise<FastifyInstance> {
-    const app = Fastify({
-        // logger: {
-        //     level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
-        // },
+  //register all plugins
+  //serve static files
+  await app.register(fastifyStatic, viewConfig);
+
+  //register all api routes
+  //index route
+  app.get("/", async (_req, reply) => {
+    return reply.status(200).type("text/html").sendFile(Home);
+  });
+
+  // Health check endpoint
+  app.get("/api/health", async (_request, reply) => {
+    return reply.send({
+      success: true,
+      message: "API is running",
+      timestamp: new Date().toISOString(),
     });
-    //homepage route
-    app.get('/', async (req, reply) => {
-        return reply.status(200).type('text/html').send(html)
-    })
+  });
 
-    // Health check endpoint
-    app.get('/api/health', async (request, reply) => {
-        return reply.send({
-            success: true,
-            message: 'API is running',
-            timestamp: new Date().toISOString(),
-        });
+  // 404 handler
+  app.setNotFoundHandler((_request, reply) => {
+    return reply.status(404).send({
+      success: false,
+      message: "Route not found",
     });
+  });
 
-    // 404 handler
-    app.setNotFoundHandler((request, reply) => {
-        return reply.status(404).send({
-            success: false,
-            message: 'Route not found',
-        });
-    });
+  return app;
+};
 
-    // Error handler
-
-    return app;
-}
-
-export default createApp;
-
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
-    />
-    <title>Vercel + Fastify Hello World</title>
-    <meta
-      name="description"
-      content="This is a starter template for Vercel + Fastify."
-    />
-  </head>
-  <body>
-    <h1>Vercel + Fastify Hello World</h1>
-  </body>
-</html>
-`
+export default App;
